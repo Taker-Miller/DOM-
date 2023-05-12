be54f4c42c24813c9189bb92390aff524ceeed69
Takerin <96486976+Taker-Miller@users.noreply.github.com>
2023-05-12 03:37:21 -0400

    Add files via upload

---
+++ Nueva carpeta/main.js
@@ -0,0 +1,9 @@
+window.onload = () => { /*lo que hace window.onload hace esperar a que el sitio web
+se encuentre completamente cargado*/
+
+
+console.log('Hola, intro a dom')
+const parrafo = document.getElementById('text')
+console.log(parrafo.innerHTML)
+
+}
