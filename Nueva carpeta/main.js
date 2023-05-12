0b613e40a6b7d419838ebde5d53c1ca118ad4299
Takerin <96486976+Taker-Miller@users.noreply.github.com>
2023-05-12 03:49:07 -0400

    Add files via upload

--- Nueva carpeta/main.js
+++ Nueva carpeta/main.js
@@ -3,7 +3,10 @@ se encuentre completamente cargado*/
 
 
 console.log('Hola, intro a dom')
-const parrafo = document.getElementById('text')
-console.log(parrafo.innerHTML)
+const parrafo = document.getElementById('text') /*se puede obtener un elemento*/
 
+/*parrafo.innerText = 'Texto Actualizado'*/ /*y luego se puede cambiar el texto */
+
+parrafo.innerHTML = '<li>Elemento 1 </li> <li> Elemento 2</li>' /*Agregar elementos
+html a otro elemento html*/
 }
